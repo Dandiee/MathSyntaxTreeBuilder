ea2711b3b91b5527bd7f0853e677f07fd38c6e88
Daniel_Koczur <daniel_koczur@epam.com>
2023-09-29 12:18:58 +0200

    add evaluation

--- MathSyntaxTreeBuilder/Program.cs
+++ MathSyntaxTreeBuilder/Program.cs
@@ -91,20 +91,26 @@ public class Program
 
     public static void Test()
     {
-        Test("4 + (5 * 6)", "(4+(5*6))");
-        Test("(4 + 5) * 6", "((4+5)*6)");
-        Test("4 + 5 * 6", "(4+(5*6))");
-        Test("4 * 5 + 6", "((4*5)+6)");
-        Test("4 + 5 - 6", "((4+5)-6)");
-        Test("4 + 5 + 6", "((4+5)+6)");
-        Test("4 + 5", "(4+5)");
+        Test("4 + (5 * 6)", "(4+(5*6))", 34);
+        Test("(4 + 5) * 6", "((4+5)*6)", 54);
+        Test("4 + 5 * 6", "(4+(5*6))", 34);
+        Test("4 * 5 + 6", "((4*5)+6)", 26);
+        Test("4 + 5 - 6", "((4+5)-6)", 3);
+        Test("4 + 5 + 6", "((4+5)+6)", 15);
+        Test("4 + 5", "(4+5)", 9);
 
     }
 
-    public static void Test(string input, string expectedOutput)
+    public static void Test(string input, string expectedOutput, double? expectedEval)
     {
         var result = GetSyntaxTree(input);
         var output = result.BuildString();
+        if (expectedEval.HasValue)
+        {
+            var eval = result.Eval();
+            Debug.Assert(Math.Abs(eval - expectedEval.Value) < 0.0001d);
+        }
+        
 
         Debug.Assert(output == expectedOutput);
     }
@@ -116,6 +122,7 @@ public class Program
     {
         public Node? Parent { get; set; }
         public abstract string BuildString();
+        public abstract double Eval();
     }
 
     [DebuggerDisplay("{Op.Name}")]
@@ -140,20 +147,24 @@ public class Program
         public override string ToString() => Op.Name;
 
         public override string BuildString() => $"({Op.ToStringFunc(this)})";
+        public override double Eval() => Op.EvalFunc(this);
     }
 
     [DebuggerDisplay("{Value}")]
     public class OpArgNode : Node
     {
         public readonly string Value;
+        public readonly double DoubleValue;
 
         public OpArgNode(string value)
         {
             Value = value;
+            DoubleValue = double.Parse(value);
         }
 
         public override string ToString() => Value;
         public override string BuildString() => Value;
+        public override double Eval() => DoubleValue;
     }
 
     public struct Op
@@ -161,24 +172,36 @@ public class Program
         public static readonly Dictionary<string, Op> ByKeys = new();
 
         public static readonly Op Add = new("+", 1, 2,
+            node => node.Children[0].Eval() + node.Children[1].Eval(),
             node => $"{node.Children[0].BuildString()}+{node.Children[1].BuildString()}");
 
         public static readonly Op Subtract = new("-", 1, 2,
+            node => node.Children[0].Eval() - node.Children[1].Eval(),
             node => $"{node.Children[0].BuildString()}-{node.Children[1].BuildString()}");
 
         public static readonly Op Mul = new("*", 2, 2,
+            node => node.Children[0].Eval() * node.Children[1].Eval(),
             node => $"{node.Children[0].BuildString()}*{node.Children[1].BuildString()}");
 
         public static readonly Op Div = new("/", 2, 2,
+            node => node.Children[0].Eval() / node.Children[1].Eval(),
             node => $"{node.Children[0].BuildString()}/{node.Children[1].BuildString()}");
+
+        public static readonly Op Exp = new("^", 3, 2,
+            node => Math.Pow(node.Children[0].Eval(), node.Children[1].Eval()),
+            node => $"{node.Children[0].BuildString()}^{node.Children[1].BuildString()}");
 
         public readonly int Precedent;
         public readonly string Name;
         public readonly int OperandsCount;
         public readonly Func<OpNode, string> ToStringFunc;
+        public readonly Func<OpNode, double> EvalFunc;
 
-        public Op(string name, int precedent, int operandsCount, Func<OpNode, string> toStringFunc)
+        public Op(string name, int precedent, int operandsCount, 
+            Func<OpNode, double> evalFunc,
+            Func<OpNode, string> toStringFunc)
         {
+            EvalFunc = evalFunc;
             Name = name;
             Precedent = precedent;
             OperandsCount = operandsCount;
